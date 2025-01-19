87692e34217a2df4073c4b979dd21cafe0a78e7c
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 19:59:13 -0800

    enable platforms

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,13 @@
-set -x
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
 
 ./configure --prefix="${PREFIX}"
 make
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
 make check
+fi
 make install
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
 make installcheck
+fi
